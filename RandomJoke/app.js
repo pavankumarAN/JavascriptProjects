de51a802f40af7fd3661c320194c2a89d4651d92
pavankumarAN <pavankumaran51@gmail.com>
2021-06-05 18:09:20 +0530

    Finished with Promise approach

--- RandomJoke/app.js
+++ RandomJoke/app.js
@@ -6,29 +6,42 @@ const img = document.querySelector('img');
 const url = `https://api.chucknorris.io/jokes/random`;
 
 btn.addEventListener('click', () => {
-    loadJoke(url);
+    loadJoke(url)
+        .then((response) => displayData(response))
+        .catch(err => console.log(err));
 });
 
 function loadJoke(url) {
-    const xhr = new XMLHttpRequest();
-    console.log(xhr);
+    return new Promise((res, rej) => {
+        const xhr = new XMLHttpRequest();
+        console.log(xhr);
 
-    xhr.open('GET', url);
-    console.log(xhr);
+        xhr.open('GET', url);
+        console.log(xhr);
 
-    xhr.onreadystatechange = () => {
-        if (xhr.status === 200 && xhr.readyState === 4) {
-            console.log(xhr);
-            const resObject = JSON.parse(xhr.responseText);
-            console.log(resObject);
-            img.src = resObject.icon_url;
-            container.appendChild(img);
-            h2.innerText = `Joke is created on - ${new Date(resObject.created_at).toDateString()}`;
-            container.appendChild(h2);
-            
-            p.innerText = resObject.value;
-            container.appendChild(p);
+        xhr.onreadystatechange = () => {
+            if (xhr.status === 200 && xhr.readyState === 4) {
+                res(xhr);
+            } else {
+                console.log({
+                    status: xhr.status,
+                    text: xhr.statusText
+                })
+            }
         }
-    }
-    xhr.send();
+        xhr.send();
+    });
+}
+
+function displayData(data) {
+    console.log(data);
+    const resObject = JSON.parse(data.responseText);
+    console.log(resObject);
+    img.src = resObject.icon_url;
+    container.appendChild(img);
+    h2.innerText = `Joke is created on - ${new Date(resObject.created_at).toDateString()}`;
+    container.appendChild(h2);
+
+    p.innerText = resObject.value;
+    container.appendChild(p);
 }
